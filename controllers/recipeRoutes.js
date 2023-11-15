e55110e5c7c4b3b5aabc81ce7b2d1a604401cb46
cjenschke <cimarron13@mac.com>
2023-11-14 18:41:35 -0600

    error fix

---
+++ controllers/recipeRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/recipe/:id', (req, res) => {
+    res.render('single-recipe', { id: req.params.id });
+});
+
+module.exports = router;
