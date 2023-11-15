a9b1186d04b48724b7831489316ba87ee9967920
Bosh <grayd500@gmail.com>
2023-11-14 23:33:32 -0500

    DRY the Views to be more efficient -- added some hard-coded mock recipes to routes for testing views

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,8 +2,19 @@
 const express = require('express');
 const router = express.Router();
 
+// Dashboard page route
 router.get('/dashboard', (req, res) => {
-    res.render('dashboard');
+    // Mock data for favoriteRecipes for testing purposes
+    const favoriteRecipes = [
+        { id: 1, title: "Spaghetti Carbonara" },
+        { id: 2, title: "Chicken Alfredo" },
+        { id: 3, title: "Caesar Salad" },
+        // ... more mock recipes
+    ];
+
+    // Here, you would also include logic to check if the user is logged in
+    res.render('dashboard', { favoriteRecipes });
 });
 
 module.exports = router;
+
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,15 @@
 const express = require('express');
 const router = express.Router();
 
+// Sample data for testing
+const recentRecipes = [
+    { id: 1, title: "Frog Spleens in Tomato Sauce" },
+    { id: 2, title: "Cream of Goat Hair" },
+    { id: 3, title: "Toad Meat Souffle" },
+    { id: 4, title: "Uncooked Wheat Chaff in Water" },
+    { id: 5, title: "Cricket Heads in Squid Ink" }
+];
+
 // Home page route
 router.get('/', (req, res) => {
     res.render('homepage');
@@ -17,10 +26,6 @@ router.get('/registration', (req, res) => {
     res.render('registration');
 });
 
-// Dashboard page route
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard');
-});
-
 module.exports = router;
+
 
