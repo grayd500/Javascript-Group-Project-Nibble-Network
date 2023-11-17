7ba64de94c96bbf07cf5e354a18d44e875a59253
Bosh <grayd500@gmail.com>
2023-11-16 19:59:40 -0500

    Update with front-end logout function setup on navbar

---
+++ public/js/logout.js
@@ -0,0 +1,22 @@
+// public/js/logout.js:
+document.addEventListener('DOMContentLoaded', function () {
+    const logoutLink = document.getElementById('logout');
+
+    logoutLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        fetch('/logout', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+        })
+        .then(response => {
+            if (response.ok) {
+                location.href = '/login';
+            } else {
+                alert('Logout failed');
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    });
+});
